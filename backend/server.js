c414440d07deff46c4ca6b33cca2ce6a0d908f06
Hemalatha N <hema3021@global.chainsys.com>
2022-06-13 11:59:13 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dbconnection =require('./nanodb');
-const app = express();
+let app = express();
 const port = 8000;
 app.use(express.static('public'));
 
